be45080e6db5f749c8740d8a484e3e438512f78f
CelienFio <celien.fiorelli@ynov.com>
2024-10-16 16:15:01 +0200

    init back

---
+++ Back/app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $user = $request->validate([
+            'email' => 'required|email|exists:'.(new User())->getTable().',email',
+            'password' => 'required|min:4',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'token' => $user->createToken(time())->plainTextToken,
+                'user' => $user,
+            ]);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|unique:users,username',
+            'email' => 'required|email|unique:users,email', //TODO verifier le mail
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'token' => $user->createToken(time())->plainTextToken,
+            'user' => $user,
+        ]);
+    }
+
+    public function destroy()
+    { 
+        Auth::guard('web')->logout();
+ 
+        return response(null, 204);
+     }
+}
---
+++ Back/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::prefix('/')->middleware('auth:sanctum')->group(function () {
+    Route::delete('logout', [AuthController::class, 'destroy']);
+});
+
+
+Route::prefix('/')->group(function () {
+    Route::post('register', [AuthController::class, 'store']);
+    Route::post('login', [AuthController::class, 'authenticate']);
+});
