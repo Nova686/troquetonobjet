cd00248c649a45b2484ccfcc36146b9f4960fa01
Roarkiner <dujour.lenaic@gmail.com>
2024-10-16 11:56:55 +0200

    Ionic and React setup

---
+++ Front/troque-ton-objet/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'troque-ton-objet',
+  webDir: 'dist'
+};
+
+export default config;
