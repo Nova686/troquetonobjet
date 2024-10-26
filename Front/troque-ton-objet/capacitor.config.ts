ffb0654b9a021a9fff86047e29a8367ca531ee58
Roarkiner <dujour.lenaic@gmail.com>
2024-10-26 16:35:56 +0200

    Abandon de Ionic au profit de React + Material UI

--- Front/troque-ton-objet/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'troque-ton-objet',
-  webDir: 'dist'
-};
-
-export default config;
