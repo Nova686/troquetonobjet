7bf94274ed2d43d5256ed97b60528dde07b89167
jetonpeche <nicolas.np63@gmail.com>
2024-12-18 15:50:35 +0100

    google place api => route

--- Back/routes/guest.php
+++ Back/routes/guest.php
@@ -4,14 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OfferController;
 use App\Http\Controllers\GooglePlaceController;
 
-Route::prefix('/')->group(function () {
-    // Route for Offers
-    Route::prefix('offers')->group(function () {
-        Route::get('', [OfferController::class, 'getOffers']);
-        Route::get('{offer}', [OfferController::class, 'get']);
-    });
-
-    Route::get("test/{searchTerm}", [GooglePlaceController::class, "AutoComplete"]);
-    Route::get("test2/{placeId}", [GooglePlaceController::class, "Location"]);
-
+// Route for Offers
+Route::prefix('offers')->group(function () {
+    Route::get('', [OfferController::class, 'getOffers']);
+    Route::get('{offer}', [OfferController::class, 'get']);
 });
+
+Route::get("auto-complete/{searchTerm}", [GooglePlaceController::class, "AutoComplete"]);
