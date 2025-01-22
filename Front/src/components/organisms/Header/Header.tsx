91deecfe97676bf8d96fa5505a2d1bce7d1d7a10
Lord-forgos <johan.rousseau39@gmail.com>
2025-01-22 09:48:12 +0100

    changements de celien

--- Front/src/components/organisms/Header/Header.tsx
+++ Front/src/components/organisms/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header: FC = () => {
 	const { isConnected, user } = useAuth();
 
     return (
-        <nav className="main-navbar">
+        <nav className="main-navbar" style={{ zIndex: 99 }}>
             <ul>
                 <li><Link to="/"><h3>Accueil</h3></Link></li>
                 <li><Link to="/offers"><h3>Liste des offres</h3></Link></li>
