e833e7e18ea80b977450fbe894cea702b484c46b
yehyaabk <yehyaaboukhechfe.129@gmail.com>
2025-01-22 10:29:40 +0200

    fix pull dev

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import {OfferForm} from "./components/organisms";
 import {Container} from "@mui/material";
+import AccountPage from './account-page';
 
 function App() {
   return (
       <Container maxWidth="sm">
-        <OfferForm/>
+        <AccountPage/>
       </Container>
   );
 }
