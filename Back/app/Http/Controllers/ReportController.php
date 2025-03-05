6ce8093b40f8793ddc23e27c380acfbf0ef25da6
Tlassalle0 <tiborlassalle@gmail.com>
2025-03-05 13:59:58 +0100

    reports

--- Back/app/Http/Controllers/ReportController.php
+++ Back/app/Http/Controllers/ReportController.php
@@ -4,8 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Report;
+use App\Http\Resources\ReportResource;
 
 class ReportController extends Controller
 {
-    //
+    function create(Request $request){
+        $request->validate([
+            "user_id"=>"required",
+            "offer_id"=>"required",
+            "reason"=>"required",
+        ]);
+
+        $existingReport = Report::where('user_id', $request->user_id)
+                            ->where('offer_id', $request->offer_id)
+                            ->first();
+
+        if ($existingReport) {
+            $existingReport->delete();
+            return response()->json([
+                "message" => "Report removed successfully",
+            ]);
+        }
+
+        $report = new Report();
+        $report->user_id = $request->user_id;
+        $report->offer_id = $request->offer_id;
+        $report->reason = $request->reason;
+        $report->save();
+        return response()->json([
+            "report"=>ReportResource::make($report),
+        ]);
+    }
+
+    function getReports(Request $request){
+        $reports = Report::all();
+        return response()->json([
+            "reports"=>ReportResource::collection($reports),
+        ]);
+    }
+
+    function delete(Request $request, $id){
+        $report = Report::find($id);
+        $report->delete();
+        return response()->json([
+            "message"=>"Report deleted successfully",
+        ]);
+    }
 }
