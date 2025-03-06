5fa6957d95301499f7307017e0058321f6428ba3
Lord-forgos <johan.rousseau39@gmail.com>
2025-03-06 12:12:55 +0100

    ajout sommaire de la page des details d une offre

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Layout from './components/templates/Layout/Layout';
-import {Home, Offers, CreateOffer, Authentication, Conversations} from './components/pages';
+import {Home, Offers, CreateOffer, Authentication, Conversations, DetailOffer} from './components/pages';
 import ProtectedRoute from './components/shared/ProtectedRoute';
 import Profile from './components/pages/Profile/Profile';
 import theme from './theme';
@@ -35,6 +35,7 @@ function App() {
                             }/>
 
                             <Route path='/offers/form' element={<CreateOffer/>}/>
+                            <Route path='/offers/:id' element={<DetailOffer/>}/>
                         </Route>
                     </Routes>
                 </BrowserRouter>
---
+++ Front/src/components/pages/Offer/DetailOffer.tsx
@@ -0,0 +1,72 @@
+import {FC, useEffect, useState} from "react";
+import {createRequestHandler, useParams} from "react-router-dom";
+import {Offer} from "../../../typings/Offer";
+import axiosService from "../../../services/AxiosService";
+import {AxiosResponse} from "axios";
+import {Typography} from "../../atoms";
+import {FavoriteButton} from "../../molecules";
+import {useTheme} from "@mui/material/styles";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import {dateFormat} from "../../../services/FormatterService";
+
+const DetailOffer: FC = () =>
+{
+    const {id} = useParams();
+    const [offer, setOffer] = useState<Offer | null>(null);
+    const theme = useTheme();
+
+    const handleOffer: () => Promise<void> = async (): Promise<void> =>
+    {
+        const response: AxiosResponse<any, any> = await axiosService.get(`offers/${id}`)
+        setOffer(response.data);
+
+        console.log(response.data)
+    }
+
+    useEffect((): void =>
+    {
+        handleOffer();
+    }, []);
+
+    return (
+        !offer ? null :
+            <Typography component={"div"} sx={{display: "flex"}}>
+                <Typography component={"div"} sx={{width: "70%", display: "flex", gap: "8px", flexDirection: "column"}}>
+                    <Typography component={"div"} sx={{
+                        display:        "flex",
+                        justifyContent: "space-between",
+                        alignItems:     "center",
+                        color:          theme.palette.primary.main
+                    }}>
+                        <Typography component={"span"} sx={{
+                            fontSize:   "28px",
+                            fontWeight: "bold"
+                        }}>{offer.title}</Typography>
+                        <FavoriteButton offerId={offer.id} defaultFilled={offer.isFavorite}/>
+                    </Typography>
+                    <Typography sx={{
+                        display:    'flex',
+                        fontSize:   '12px',
+                        alignItems: "center",
+                        color:      theme.palette.secondary.main
+                    }}>
+                        <CalendarMonthIcon/>Le {dateFormat(offer.createdAt)}
+                    </Typography>
+                    <Typography component={"span"} sx={{fontSize: "18px", color: theme.palette.primary.main}}>
+                        {offer.description}
+                    </Typography>
+                </Typography>
+                <Typography component={"div"} sx={{width: "30%"}}>
+                    <Typography component={"div"} sx={{display: "flex"}}>
+                        <Typography component={"div"} sx={{width: "50%"}}></Typography>
+                        <Typography component={"div"} sx={{width: "50%"}}>
+                            <Typography sx={{fontSize: "28px", color: theme.palette.primary.main}}>{offer.author.username}</Typography>
+                        </Typography>
+                    </Typography>
+                </Typography>
+            </Typography>
+    );
+
+}
+
+export default DetailOffer;
