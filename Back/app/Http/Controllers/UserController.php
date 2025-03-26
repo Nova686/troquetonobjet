cf210dcc9b231d1ef43eff8890b8bad3ac3414cd
jetonpeche <nicolas.np63@gmail.com>
2025-03-26 14:23:15 +0100

    api => supprimer utilisateur + delete cascade sur user id et offer id

--- Back/app/Http/Controllers/UserController.php
+++ Back/app/Http/Controllers/UserController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserUpdateRequest;
 use App\Library\Results;
+use App\Library\Storage\StorageService;
+use App\Models\Offer;
+use App\Models\OfferImage;
 use App\Models\User;
+use App\Models\UserAddress;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    public function __construct(private StorageService $storageServ) { }
+
     public function update(UserUpdateRequest $request)
     {
         $data = $request->validated();
@@ -18,5 +24,20 @@ class UserController extends Controller
         return $nb > 0 ? Results::noContent() : Results::notFound();
     }
 
-    
+    public function delete()
+    {
+        $userId = Auth::user()->id;
+
+        $urlFileList = OfferImage::query()
+            ->join(
+                "offer as o",
+                "o.id", "=", "offer_images.offer_id"
+            )
+            ->pluck("url")
+            ->toArray();
+
+        $this->storageServ->delete($urlFileList, "public");
+
+        User::where("id", $userId)->delete();
+    }
 }
