53c71928683e4e669acd3e86f5ffb14f05fedb5f
jetonpeche <nicolas.np63@gmail.com>
2025-03-26 10:07:47 +0100

    Update Offer.php

--- Back/app/Models/Offer.php
+++ Back/app/Models/Offer.php
@@ -51,11 +51,6 @@ class Offer extends Model
         return $this->updated_at->getTimestamp() !== $this->created_at->getTimestamp();
     }
 
-    public function getUrlAttribute(): bool
-    {
-        return $this->url->getTimestamp();
-    }
-
     public function scopeIsVisible(Builder $query, bool $isVisible = true)
     {
         return $query->where('is_visible', $isVisible);
