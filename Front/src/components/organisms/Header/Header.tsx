dfd112a2ceccffa4f3265f515ebefdb0a2ed9b75
Roarkiner <dujour.lenaic@gmail.com>
2025-04-15 19:14:42 +0200

    Header - Mon compte

--- Front/src/components/organisms/Header/Header.tsx
+++ Front/src/components/organisms/Header/Header.tsx
@@ -62,7 +62,7 @@ const Header: FC = () => {
                             <Box display='flex' alignItems='center' flexDirection={"column"}
                                 color={theme.palette.custom.textColor}>
                                 <AccountCircleIcon/>
-                                {user?.username}
+                                Mon compte
                             </Box>
                         </Link>
                     </>
